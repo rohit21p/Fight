5fc942ae5a07ca90e888c62104d7ad86d3485c95
Rohit <iamrohit.panjwani@gmail.com>
2019-08-30 19:16:01 +0530

    players can enter the fight

--- contracts/Fight.sol
+++ contracts/Fight.sol
@@ -1,5 +1,10 @@
 pragma solidity ^0.4.17;
 
 contract Fight {
+    address[] public players;
     
+    function enter() public payable{
+        require(msg.value > 0.1 ether);
+        players.push(msg.sender);
+    }
 }
