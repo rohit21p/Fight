f82d17ac3b1204d6cffd559f9070fe40c4f10842
Rohit <iamrohit.panjwani@gmail.com>
2019-08-30 19:18:07 +0530

    manager identity saved

--- contracts/Fight.sol
+++ contracts/Fight.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.4.17;
 
 contract Fight {
     address[] public players;
+    address public manager;
+
+    function Fight() public {
+        manager = msg.sender;
+    }
     
     function enter() public payable{
         require(msg.value > 0.1 ether);
